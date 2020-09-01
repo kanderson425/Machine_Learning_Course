c076b2488da8694570cec0e6dd2f4aac61fde908
kanderson425 <kyleanderson.dev@gmail.com>
2020-09-01 08:25:44 -0500

    Adds the aTools package, the dataset splitting and the feature scaling in R for the data.csv.

--- Machine Learning A-Z (Codes and Datasets)/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/R/NOTES_data_preprocessing_template.R
+++ Machine Learning A-Z (Codes and Datasets)/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/R/NOTES_data_preprocessing_template.R
@@ -1,4 +1,36 @@
 # Data Preprocessing
 
 # Importing the dataset
-dataset = read.csv('Data.cc')
+dataset = read.csv('Data.csv')
+
+# Taking care of missing data
+dataset$Age = ifelse(is.na(dataset$Age), 
+                     ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
+                     dataset$Age)
+
+dataset$Salary = ifelse(is.na(dataset$Salary), 
+                     ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
+                     dataset$Salary)
+
+# Encoding Categorical Data
+dataset$Country = factor(dataset$Country,
+                         levels = c('France', 'Spain', 'Germany'),
+                         labels = c(1, 2, 3))
+
+dataset$Purchased = factor(dataset$Purchased,
+                         levels = c('No', 'Yes'),
+                         labels = c(0, 1))
+
+# Splitting the dataset into the Training set and Test set
+install.packages('bitops')
+install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Purchased, SplitRatio = 0.8)
+
+
+
+
+
+
+
