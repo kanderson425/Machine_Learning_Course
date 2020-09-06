934d7949640365020e7c68f1c40c8af95a9778e4
kanderson425 <kyleanderson.dev@gmail.com>
2020-09-06 17:56:24 -0500

    Adds the data import and the polynomial regresssion model creataion for the Position_Salaries dataset.

--- Machine Learning A-Z (Codes and Datasets)/Part 2 - Regression/Section 5 - Multiple Linear Regression/R/NOTES_multiple_linear_regression.R
+++ Machine Learning A-Z (Codes and Datasets)/Part 2 - Regression/Section 5 - Multiple Linear Regression/R/NOTES_multiple_linear_regression.R
@@ -16,5 +16,31 @@ training_set = subset(dataset, split == TRUE)
 test_set = subset(dataset, split == FALSE)
 
 
-# training_set = scale(training_set)
-# test_set = scale(test_set)
+# Fitting muultiple linear regression to the Training set
+regressor = lm(formula = Profit ~ .,
+               data = training_set)
+
+# Predicting the Test set results
+y_pred = predict(regressor, newdata = test_set)
+
+# Building the optimal model using Backward Elimination
+regressor = lm(formula = Profit ~ R.D.Spend + Administration + Marketing.Spend + State,
+               data = dataset)
+summary(regressor)
+
+# Removing the state variable
+regressor = lm(formula = Profit ~ R.D.Spend + Administration + Marketing.Spend,
+               data = dataset)
+summary(regressor)
+
+# Removing the Administration variable
+regressor = lm(formula = Profit ~ R.D.Spend + Marketing.Spend,
+               data = dataset)
+summary(regressor)
+
+# Removing the marketing spend variable
+regressor = lm(formula = Profit ~ R.D.Spend,
+               data = dataset)
+summary(regressor)
+
+
